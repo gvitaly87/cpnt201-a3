c5bdca6a0f18f759d409725885ac50ea79fde520
Vitaly Gins <vitaly.gins@gmail.com>
2021-02-15 18:17:12 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# CPNT201 - Assignment 3 - Raster Image Optimization
+## General Information
+* Course: CPNT-201 Web Design Tools and Techniques
+* Author: [Vitaly Gins](https://github.com/gvitaly87) - Repository owner and maintainer.
+* Github Links:
+  * [GH repo](https://github.com/gvitaly87/cpnt201-a3)
+  * [GH Pages site](https://gvitaly87.github.io/cpnt201-a3/)
+
+### Context
+A pocket watch company required a logo. Pocket watches bring both style and accuracy, and so should their logo. The logo is designed so that the user can customize its color scheme. The logo is also showing the exact time on the users computer by animating the SVG.
+
+The color is adjusted using three main buttons. One button controls the watch's body, one controls the face, and the last controls the watch's hands. The watch animation is done using the transform rotate property which updates at a set interval(every second). 
+
+---
+## Attributions
+### SVG
+* [SVGOMG](https://jakearchibald.github.io/svgomg/) helped optimize the SVG included on the assignment
+* [Figma](https://www.figma.com/files/recent) was used in order to create the SVG
---
+++ js/script.js
@@ -0,0 +1,55 @@
+/*Get the watch's hands from the SVG markup*/
+const hourHand = document.querySelector('#hour');
+const minHand = document.querySelector('#minute');
+const secHand = document.querySelector('#second');
+/* Set the actual time */
+const date = new Date();
+const hr = date.getHours();
+const min = date.getMinutes();
+const sec = date.getSeconds();
+/*Adjust the hand position*/
+let hrPosition = (hr * 360 / 12) + (min * (360 / 60) / 12);
+let minPosition = (min * 360 / 60) + (sec * (360 / 60) / 60);
+let secPosition = sec * 360 / 60;
+
+/* Updates the watch hands each time its called */
+function updateWatch() {
+    /* Need to adjust the hr/min arm to move when the second moves */
+    /* 360deg / 12hrs = 360deg / (12*60*60)sec = 1/120 */
+    hrPosition = hrPosition + (1 / 120);
+    /* 360deg / 60mins = 360deg/3600sec = 1/10 */
+    minPosition = minPosition + (1 / 10);
+    /* 360deg / 60seconds = 6deg per second */
+    secPosition = secPosition + 6;
+
+    hourHand.style.transform = "rotate(" + hrPosition + "deg)";
+    minHand.style.transform = "rotate(" + minPosition + "deg)";
+    secHand.style.transform = "rotate(" + secPosition + "deg)";
+}
+
+const interval = setInterval(updateWatch, 1000);
+
+/*Color Change */
+const watchColor = document.querySelector('#watch-color');
+const faceColor = document.querySelector('#face-color');
+const handColor = document.querySelector('#hand-color');
+
+function changeColor(cssVar) {
+    const root = document.querySelector(':root');
+    const randomColor = Math.floor(Math.random()*16777215).toString(16);
+    
+    console.log(cssVar,randomColor);
+    root.style.setProperty(cssVar, `#${randomColor}`);
+}
+
+watchColor.addEventListener('click', function(){
+    changeColor('--watch-body-color')
+});
+
+faceColor.addEventListener('click', function(){
+    changeColor('--watch-face-color')
+});
+
+handColor.addEventListener('click', function(){
+    changeColor('--handle-color')
+});
